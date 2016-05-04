074a9e2a14190dfc1aceca0de99745cc911f8d7a
oyvindhermansen <oyvind@ludensreklame.no>
2016-05-04 15:11:33 +0200

    deleted ajax.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,1 @@
 import { mQ } from './lib/miniQuery'
-
-mQ('h1')
-  .addClass('hey')
-  .removeClass('hey')
-  .addClass('lol')
-  .on('click', () => {
-    if (mQ('h1').hasClass('lol')) {
-      mQ('h1').hide()
-    }
-  })
-
-mQ('button').on('click', () => {
-  mQ('h1').show()
-})
